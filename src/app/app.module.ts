431a88b8b679b1b7d8395a8a8780726dc04af7d1
Ardra Salemy <ardrasalemy@gmail.com>
2019-02-20 19:10:42 -0500

    Adds a couple components and pulls random drink API.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DrinkListComponent } from './drink-list/drink-list.component';
+import { DrinkService } from './service/drink.service';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrinkListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    DrinkService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/drink-list/drink-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DrinkService } from '../service/drink.service';
+
+@Component({
+  selector: 'app-drink-list',
+  templateUrl: './drink-list.component.html',
+  styleUrls: ['./drink-list.component.css']
+})
+export class DrinkListComponent implements OnInit {
+
+  constructor(private ds: DrinkService) { }
+
+  ngOnInit() {
+  }
+
+}
